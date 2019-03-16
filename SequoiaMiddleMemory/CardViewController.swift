9189dddb8bf5fe1aa2a2500ba1bf060d6c76b7b1
Ogden Kent <ogkent@microsoft.com>
2019-03-16 10:18:07 -0700

    Initial commit

---
+++ SequoiaMiddleMemory/CardViewController.swift
@@ -0,0 +1,139 @@
+//
+//  CardViewController.swift
+//  MtDiabloMemory
+//
+//  Created by Ogden Kent on 3/13/19.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+  
+  var number: Int? {
+    didSet {
+      if let number = number {
+        backNumberLabel.text = String(number)
+      } else {
+        backNumberLabel.text = nil
+      }
+    }
+  }
+  
+  var message: String? {
+    didSet {
+      frontLabel.text = message
+    }
+  }
+  
+  var tapCallback: ((CardViewController) -> ())?
+  
+  var isRevealed: Bool {
+    get {
+      return back.isHidden
+    }
+  }
+  
+  private let back: UIView
+  private let front: UIView
+  private let backImageView: UIImageView
+  private let backNumberLabel: UILabel
+  private let frontLabel: UILabel
+  
+  convenience init() {
+    self.init(nibName:nil, bundle:nil)
+  }
+  
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    
+    back = UIView(frame: CGRect.zero)
+    back.backgroundColor = UIColor(patternImage: UIImage(named: "paper")!)
+    
+    front = UIView(frame: CGRect.zero)
+    front.backgroundColor = UIColor.white
+    front.isHidden = true
+    
+    backImageView = UIImageView(image: UIImage(named: "tree")!)
+    backImageView.contentMode = .scaleAspectFit
+    back.addSubview(backImageView)
+    
+    backNumberLabel = UILabel(frame: CGRect.zero)
+    backNumberLabel.font = UIFont.systemFont(ofSize: 30, weight: .regular)
+    backNumberLabel.textColor = UIColor(hexString: "#333333")
+    backNumberLabel.textAlignment = NSTextAlignment.center
+    back.addSubview(backNumberLabel)
+    
+    frontLabel = UILabel(frame: CGRect.zero)
+    frontLabel.font = UIFont.systemFont(ofSize: 80)
+    frontLabel.textAlignment = NSTextAlignment.center
+    front.addSubview(frontLabel)
+    
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    back.layer.cornerRadius = 10;
+    back.layer.masksToBounds = true;
+    
+    front.layer.cornerRadius = 10;
+    front.layer.masksToBounds = true;
+    
+    view.layer.shadowColor = UIColor.black.cgColor
+    view.layer.shadowOpacity = 0.5
+    view.layer.shadowRadius = 5.0
+    view.layer.shadowOffset = CGSize(width: 0, height: 7)
+    
+    view.addSubview(back)
+    view.addSubview(front)
+    
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
+    back.addGestureRecognizer(tapGesture)
+  }
+  
+  override func viewDidLayoutSubviews() {
+    back.frame = view.bounds
+    front.frame = view.bounds
+    
+    backImageView.frame = back.bounds.insetBy(dx: 20, dy: 20)
+    backNumberLabel.frame = CGRect(x: back.frame.size.width - 50.0, y: back.frame.size.height - 50.0, width: 50, height: 50)
+    frontLabel.frame = front.bounds
+  }
+  
+  @objc func tapped(_ sender: UIGestureRecognizer) {
+    tapCallback?(self)
+  }
+  
+  func revealFront() {
+    UIView.transition(from: back, to: front, duration: 0.4, options: [.showHideTransitionViews, .transitionFlipFromRight])
+  }
+  
+  func reset() {
+    UIView.transition(from: front, to: back, duration: 0.4, options: [.showHideTransitionViews, .transitionFlipFromLeft])
+  }
+  
+  func indicateSuccess() {
+    
+    let pulseAnimation = CABasicAnimation(keyPath: "transform.scale")
+    pulseAnimation.beginTime = CACurrentMediaTime() + 0.8
+    pulseAnimation.duration = 0.3
+    pulseAnimation.toValue = NSNumber(value: 1.1)
+    pulseAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+    pulseAnimation.autoreverses = true
+    view.layer.add(pulseAnimation, forKey: nil)
+    
+    let colorAnimation = CABasicAnimation(keyPath: "backgroundColor")
+    colorAnimation.beginTime = CACurrentMediaTime() + 0.8
+    colorAnimation.duration = 0.3
+    colorAnimation.toValue = UIColor(hexString: "#ffef8c").cgColor
+    colorAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+    colorAnimation.autoreverses = true
+    frontLabel.layer.add(colorAnimation, forKey: nil)
+    
+  }
+  
+}
--- SequoiaMiddleMemory/ViewController.swift
+++ SequoiaMiddleMemory/ViewController.swift
@@ -2,19 +2,157 @@
 //  ViewController.swift
 //  SequoiaMiddleMemory
 //
-//  Created by Ogden Kent on 3/16/19.
-//  Copyright © 2019 Ogden Kent. All rights reserved.
+//  Created by Ogden Kent on 3/13/19.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+  
+  let rows = 3
+  let cols = 6
+  let spacing = Float(30)
 
+//  let rows = 2
+//  let cols = 3
+//  let spacing = Float(100)
+  
+  let messages:[String] = [
+    "🌭", "👻", "🙅🏾‍♀️", "🧤", "🎺", "🏔", "🕹", "🛸", "🎯", "🧯", "🧟‍♂️", "🕺🏻", "🧵", "🐝", "🐲", "🥥", "🌯", "🏓", "🎨", "🚜", "🗿", "💳", "🧬"
+  ]
+  
+  var cards:[CardViewController] = {
+    return []
+  }()
+
+  var selectedCard1: CardViewController?
+  var selectedCard2: CardViewController?
+  
+  let backgroundImageView = UIImageView(image: UIImage(named: "table")!)
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    backgroundImageView.contentMode = .scaleToFill
+    view.addSubview(backgroundImageView)
+    
+    prepareCards()
   }
-
+  
+  override func viewDidLayoutSubviews() {
+    backgroundImageView.frame = view.bounds
+    layoutCards()
+  }
+  
+  func prepareCards() {
+    
+    let preparedMessages:[String] = {
+      let shuffedMessages = messages.shuffled()
+      var e:[String] = []
+      let count = (rows * cols / 2)
+      for i in 0..<count {
+        // always insert two messages
+        e.append(shuffedMessages[i])
+        e.append(shuffedMessages[i])
+      }
+      return e.shuffled()
+    }()
+    
+    cards.removeAll()
+    
+    for r in 0..<rows {
+      for c in 0..<cols {
+        
+        let card = CardViewController()
+        let index = (r * cols) + c
+        card.number = index+1
+        card.message = preparedMessages[index]
+        card.tapCallback = handleBackTapped
+        cards.append(card)
+        
+        addChild(card)
+        view.addSubview(card.view)
+        card.didMove(toParent: self)
+      }
+    }
+  }
+  
+  func layoutCards() {
+    
+    print("layoutCards")
+    
+    let size = self.view.frame.size
+    let cardHeight = floorf((Float(size.height) - (Float(rows) + 1) * spacing) / Float(rows))
+    let cardWidth = floorf((Float(size.width) - (Float(cols) + 1) * spacing) / Float(cols))
+    
+    for i in 0..<cards.count {
+      let xIndex = i % cols
+      let yIndex = i / cols
+      let x = (Float(xIndex + 1) * spacing) + (Float(xIndex) * cardWidth)
+      let y = (Float(yIndex + 1) * spacing) + (Float(yIndex) * cardHeight)
+      let card = cards[i]
+      card.view.frame = CGRect(x: CGFloat(x), y: CGFloat(y), width: CGFloat(cardWidth), height: CGFloat(cardHeight))
+    }
+    
+  }
+  
+  func handleBackTapped(card: CardViewController) {
+    
+    // if two cards are already selected, noop
+    if selectedCard1 != nil && selectedCard2 != nil {
+      print("Two cards already selected, wait it out...")
+      return
+    }
+    
+    // don't select the same card twice
+    if card == selectedCard1 {
+      print("Selected same card")
+      return
+    }
+    
+    print("Back tapped")
+    card.revealFront()
+    
+    if selectedCard1 != nil {
+      selectedCard2 = card
+      checkMatch()
+    } else {
+      selectedCard1 = card
+    }
+  }
+  
+  func checkMatch() {
+    
+    if (selectedCard1?.message == selectedCard2?.message) {
+      print("Found a match (\(selectedCard1!.message!))!")
+      self.selectedCard1?.indicateSuccess()
+      self.selectedCard2?.indicateSuccess()
+      self.selectedCard1 = nil
+      self.selectedCard2 = nil
+      checkWin()
+    } else {
+      print("Match not found, reset")
+      DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        self.selectedCard1?.reset()
+        self.selectedCard2?.reset()
+        self.selectedCard1 = nil
+        self.selectedCard2 = nil
+      }
+    }
+  }
+  
+  func checkWin() {
+    for card in cards {
+      if !card.isRevealed {
+        return
+      }
+    }
+    
+    print("Game has been won!")
+    // TODO
+    // show win state
+    // let user play again
+  }
 
 }
 
