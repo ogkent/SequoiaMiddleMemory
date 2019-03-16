85d311d68dd86793ebca79571f2d2c7a500b45d2
Ogden Kent <ogkent@microsoft.com>
2019-03-16 09:40:34 -0700

    Initial Commit

---
+++ SequoiaMiddleMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SequoiaMiddleMemory
+//
+//  Created by Ogden Kent on 3/16/19.
+//  Copyright © 2019 Ogden Kent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
